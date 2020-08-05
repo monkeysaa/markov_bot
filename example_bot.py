d2530e18bb3fb9c8e711ab18aa120c1d71a334d7
Alison Cordeiro <acordeiro@gmail.com>
2020-08-05 04:37:12 +0000

    Initial commit
    Co-authored-by: Aine Thompson <ainegthompson@gmail.com>

---
+++ example_bot.py
@@ -0,0 +1,60 @@
+import os
+import discord
+import sys
+from random import choice
+
+def make_chains(text_string):
+    """Take input text as string; return dictionary of Markov chains."""
+
+    chains = {}
+
+    words = text_string.split()
+    for i in range(len(words) - 2):
+        key = (words[i], words[i + 1])
+        value = words[i + 2]
+
+        if key not in chains:
+            chains[key] = []
+        chains[key].append(value)
+
+    return chains
+
+
+def make_text(chains):
+    """Take dictionary of Markov chains; return random text."""
+
+    keys = list(chains.keys())
+    key = choice(keys)
+
+    words = [key[0], key[1]]
+    while key in chains:
+        # Keep looping until we have a key that isn't in the chains
+        # (which would mean it was the end of our original text).
+
+        # Note that for long texts (like a full book), this might mean
+        # it would run for a very long time.
+
+        word = choice(chains[key])
+        words.append(word)
+        key = (key[1], word)
+
+    return ' '.join(words)
+
+
+client = discord.Client()
+token = os.environ['DISCORD_TOKEN']
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        markov_response = make_text(make_chains(message.content))
+        await message.channel.send(markov_response)
+
+client.run(token)
